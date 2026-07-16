14108af79a62780dcdf3e0c1d14c989bb5c4beb4
aldangarcia <601540@students.esic.university>
2026-07-16 16:56:27 +0200

    update type for puntuacion

---
+++ src/output/report.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+
+class InformeEmpresa(BaseModel):
+    empresa: str = Field(description="nombre de la empresa a analizar")
+    ticker: str = Field(description="indice de la empresa a analizar")
+    resumen_ejecutivo: str = Field(description="resumen ejecutivo del análisis")
+    situacion_financiera: str = Field(description="situacion financiera actual de la empresa")
+    sentimiento_reputacion: str = Field(description="sentimiento sobre la reputación de la empresa: negativo, positivo o neutro")
+    riesgos_detectados: list[str] = Field(description="lista de los riesgos detectados para la empresa")
+    conclusion: str = Field(description="conclusion del estado de la empresa para el due diligence")
+    puntuacion: float = Field(description="puntuacion del 1 al 10 para la empresa")
